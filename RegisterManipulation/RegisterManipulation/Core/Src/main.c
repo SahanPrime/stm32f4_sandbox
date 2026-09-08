0ecce3539d04e8535c8a7e229df414339e73f659
Sahan Jayarathna <gwsrj000@gmail.com>
2026-09-08 16:49:12 +0530

    OpenOCD Flashing exercise

--- RegisterManipulation/RegisterManipulation/Core/Src/main.c
+++ RegisterManipulation/RegisterManipulation/Core/Src/main.c
@@ -13,11 +13,11 @@ int main (){
 
 	//RCC and GPIOC Configurations//
 	RCC_AHB1_ENR = (RCC_AHB1_ENR|(1<<2));
-	GPIOC_MODER_BASE=(GPIOC_MODER_BASE|(10<<26));
+	GPIOC_MODER_BASE = (GPIOC_MODER_BASE & ~(3<<26)) | (1<<26);
 
 	//infinite loop//
 	while(1){
-		GPIOC_ODR_BASE= (GPIOC_ODR_BASE |(1<<13));
+		GPIOC_ODR_BASE ^= (1<<13);
 
 	}
 
