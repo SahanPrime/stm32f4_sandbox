c780e98d764829aa940b1185f408ddaec530f1ae
Sahan Jayarathna <gwsrj000@gmail.com>
2026-09-15 18:00:27 +0530

    modularized code update for LED blink using BSRR

---
+++ 5_GPIOInputOutput/Core/Src/gpio.c
@@ -0,0 +1,27 @@
+#include "gpio.h"
+
+#define GPIOCENR  (1u<<2) // GPIOC clock enable
+#define LED13_BSR  (1u<<13) // GPIOC pin 13 bit set
+#define LED13_BSS (1u<<29) // GPIOC pin 13 bit reset
+
+void led_init(void)
+{
+    // Enable GPIOC clock
+    RCC->AHB1ENR |= GPIOCENR;
+
+    // Set GPIOC pin 13 as output
+    GPIOC->MODER &= ~(3u << (13 * 2)); // Clear mode bits for pin 13
+    GPIOC->MODER |= (1u << (13 * 2));  // Set mode to output
+}
+
+void led_on(void)
+{
+    // Set GPIOC pin 13 high
+    GPIOC->BSRR = LED13_BSR;
+}
+void led_off(void)
+{
+    // Set GPIOC pin 13 low
+    GPIOC->BSRR = LED13_BSS;
+}
+
